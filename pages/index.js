6045a6ce825f6568efa9f27da1b8319072b3063a
Pablo Gomez <pgomezm1400@alumno.ipn.mx>
2021-01-14 22:35:04 -0600

    rellenado de texto

--- pages/index.js
+++ pages/index.js
@@ -11,21 +11,21 @@ export default function Home() {
         <div className="col-md-4 mb-4">
           <CardTema
             tema="Parablas Agudas"
-            descripcion="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate magni sapiente voluptate natus animi tempore!"
+            descripcion="Las palabras agudas son las que llevan acento (la intensidad de la voz) en la última sílaba."
             path="/ejercicios/palabras-agudas"
           />
         </div>
         <div className="col-md-4 mb-4">
           <CardTema
             tema="Parablas Graves"
-            descripcion="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate magni sapiente voluptate natus animi tempore!"
+            descripcion="Las palabras graves (o palabras llanas) son la que llevan la intensidad de la voz en la penúltima sílaba."
             path="/ejercicios/palabras-graves"
           />
         </div>
         <div className="col-md-4 mb-4">
           <CardTema
             tema="Parablas Esdrújulas"
-            descripcion="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate magni sapiente voluptate natus animi tempore!"
+            descripcion="Las palabras esdrújulas son las que llevan la intensidad de la voz en la antepenúltima sílaba."
             path="/ejercicios/palabras-esdrujulas"
           />
         </div>
